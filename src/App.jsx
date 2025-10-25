e5cd14e0b69df58c40cad41486b1ff28294cf637
akashpk41 <akashpk84100@gmail.com>
2025-10-25 15:28:13 +0600

    axios and other pages setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import { Route, Routes } from "react-router";
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
+import Settings from "./pages/Settings";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Routes>
-        <Route path="/" element={<h1>Home</h1>} />
-        <Route path="/login" element={<h1>Login</h1>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </div>
   );
