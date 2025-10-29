abfb23be1a91f70a6ca8e77ee20b5c8c9b2ac550
akashpk41 <akashpk84100@gmail.com>
2025-10-29 14:15:30 +0600

    profile and no chat selected component updated

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -22,8 +22,8 @@ const Profile = () => {
   };
 
   return (
-    <div className="min-h-screen bg-gradient-to-br from-base-100 to-base-200 pt-20">
-      <div className="max-w-3xl mx-auto p-4 py-8">
+    <div className=" bg-gradient-to-br from-base-100 to-base-200 ">
+      <div className="max-w-3xl mx-auto 1 py-8">
         {/* Header Section */}
         <div className="mb-8 text-center">
           <div className="flex items-center justify-center gap-3 mb-2">
@@ -60,7 +60,7 @@ const Profile = () => {
                   <img
                     src={selectedImg || authUser?.profilePic || "/avatar.png"}
                     alt="Profile"
-                    className="relative size-32 rounded-full object-cover border-4 border-primary shadow-xl"
+                    className="relative size-38 rounded-full object-cover border-4 border-primary shadow-xl"
                   />
 
                   {/* Camera Button */}
@@ -92,7 +92,7 @@ const Profile = () => {
                 </div>
 
                 <div className="flex-1 text-center sm:text-left">
-                  <h3 className="font-semibold text-lg mb-2">Update Your Photo</h3>
+                  <h3 className="font-semibold text-xl mb-2">Update Your Photo</h3>
                   <p className="text-sm text-base-content/60 mb-3">
                     {isUpdatingProfile
                       ? "Uploading your new profile picture..."
